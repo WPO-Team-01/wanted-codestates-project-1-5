1bf4498b37789891b38d0bcc7b81a81991020f73
DESKTOP-FHC4GEF\SolomonAhn <solomon0522@gmail.com>
2022-03-15 21:23:31 +0900

    fix drag motion

--- src/components/DragCanvas.js
+++ src/components/DragCanvas.js
@@ -1,8 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
-const DragCanvas = styled.canvas`
+const BaseCanvas = styled.canvas`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   background: no-repeat center/100% url("/assets/sampleImage.jpg");
+`;
+
+const DragCanvas = styled.canvas`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
 `;
 
 const DragConvas = () => {
@@ -10,7 +22,9 @@ const DragConvas = () => {
     JSON.parse(localStorage.getItem("registerItems"))
   );
   const canvasRef = useRef(null);
+  const baseCanvasRef = useRef(null);
   const [ctx, setCtx] = useState();
+  const [baseCtx, setBaseCtx] = useState();
   const [draw, setDraw] = useState(false);
 
   const [startX, setStartX] = useState();
@@ -18,11 +32,20 @@ const DragConvas = () => {
   const [endX, setEndX] = useState();
   const [endY, setEndY] = useState();
 
+  const [posX, setPosX] = useState();
+  const [posY, setPosY] = useState();
+
   useEffect(() => {
     const canvas = canvasRef.current;
+    const baseCanvas = baseCanvasRef.current;
     setCtx(canvas?.getContext("2d"));
+    setBaseCtx(baseCanvas?.getContext("2d"));
 
-    if (ctx) {
+    setPosX(canvasRef.current.getBoundingClientRect().left);
+    setPosY(canvasRef.current.getBoundingClientRect().top);
+
+    if (baseCtx) {
+      baseCtx.clearRect(0, 0, baseCtx.canvas.width, baseCtx.canvas.height);
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
       if (!registerItems) {
@@ -34,38 +57,52 @@ const DragConvas = () => {
         );
 
         initRegisterItems.forEach((elem) => {
-          ctx.strokeStyle = "rgb(34, 180, 250)";
-          ctx.lineWidth = 0.8;
-          ctx.strokeRect(
+          baseCtx.strokeStyle = "rgb(34, 180, 250)";
+          baseCtx.lineWidth = 0.8;
+          baseCtx.strokeRect(
             elem.itemStartX,
             elem.itemStartY,
             elem.itemEndX - elem.itemStartX,
             elem.itemEndY - elem.itemStartY
           );
-          ctx.fillStyle = "rgba(235, 212, 0, 0.2)";
-          ctx.fillRect(
+          baseCtx.fillStyle = "rgba(235, 212, 0, 0.2)";
+          baseCtx.fillRect(
             elem.itemStartX,
             elem.itemStartY,
             elem.itemEndX - elem.itemStartX,
             elem.itemEndY - elem.itemStartY
           );
+
+          const minX = Math.min(elem.itemStartX, elem.itemEndX);
+          const minY = Math.min(elem.itemStartY, elem.itemEndY);
+
+          baseCtx.fillStyle = "black";
+          baseCtx.font = "normal bold 20px sans-serif";
+          baseCtx.fillText(elem.itemName, minX, minY + 20);
         });
       }
     }
-  }, [ctx, registerItems, draw]);
+  }, [ctx, baseCtx, registerItems, draw]);
 
   const drawStart = (e) => {
     setDraw(true);
-    setStartX(e.clientX - ctx.canvas.offsetLeft);
-    setStartY(e.clientY - ctx.canvas.offsetTop);
+
+    console.log(e.clientX - posX, e.clientY - posY);
+
+    setStartX(e.clientX - posX);
+    setStartY(e.clientY - posY);
+    // setStartX(e.clientX - ctx.canvas.offsetLeft);
+    // setStartY(e.clientY - ctx.canvas.offsetTop);
   };
 
   const drawMove = (e) => {
     if (draw) {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-      setEndX(e.clientX - ctx.canvas.offsetLeft);
-      setEndY(e.clientY - ctx.canvas.offsetTop);
+      setEndX(e.clientX - posX);
+      setEndY(e.clientY - posY);
+      // setEndX(e.clientX - ctx.canvas.offsetLeft);
+      // setEndY(e.clientY - ctx.canvas.offsetTop);
 
       ctx.strokeStyle = "red";
       ctx.lineWidth = 0.5;
@@ -96,6 +133,9 @@ const DragConvas = () => {
         getRegisterItems.push(item);
         localStorage.setItem("registerItems", JSON.stringify(getRegisterItems));
         setRegisterItems((registerItems) => [...registerItems, item]);
+
+        baseCtx.fillStyle = "black";
+        baseCtx.fillText(registerItem, startX, startY);
       }
     }
     setDraw(false);
@@ -116,6 +156,7 @@ const DragConvas = () => {
   };
 
   return (
+    <div>
       <DragCanvas
         ref={canvasRef}
         width={720}
@@ -125,6 +166,8 @@ const DragConvas = () => {
         onMouseUp={drawEnd}
         onMouseOut={drawOut}
       />
+      <BaseCanvas ref={baseCanvasRef} width={720} height={900} />
+    </div>
   );
 };
 
