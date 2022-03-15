6779c1f8f144c39b1a7a8e4bf2ba589ff674dcd2
sun0-1106 <sy.choi1106@gmail.com>
2022-03-15 21:32:50 +0900

    pagination 완성

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,29 +1,51 @@
 import React from 'react';
 import styled from 'styled-components';
+
 const Container = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
   width: 500px;
   height: 50px;
-  border: 1px solid blue;
 `;
 
-const Button = styled.button`
-  width: 10px;
-  height: 10px;
+const Button = styled.div`
+  width: 20px;
+  height: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 5px;
+  margin-left: 10px;
+  font-size: 24px;
+  color: ${({ num, currentPage }) => (num === currentPage ? 'white' : 'black')};
+  background: ${({ num, currentPage }) =>
+    num === currentPage ? '#9f6dfa' : null};
+  border-radius: 5px;
+  box-shadow: none;
+  cursor: pointer;
+  :hover {
+    background: #ebebec;
+  }
 `;
-const Pagination = ({ postPerPage, totalPosts, paginate }) => {
+
+const Pagination = ({ postPerPage, totalPosts, paginate, currentPage }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postPerPage); i++) {
     pageNumbers.push(i);
   }
-  console.log(postPerPage, totalPosts);
+
   return (
     <div>
       <Container>
         {pageNumbers.map(num => (
-          <Button key={num}>
-            <a onClick={() => paginate(num)} href='!#' />
+          <Button
+            key={num}
+            num={num}
+            currentPage={currentPage}
+            onClick={() => paginate(num)}
+          >
             {num}
           </Button>
         ))}
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import { useLocation } from 'react-router-dom';
+import Pagination from '../components/Pagination';
 
 const Container = styled.div`
   width: 100%;
@@ -46,6 +47,15 @@ function ProductPage() {
   const regions = JSON.parse(localStorage.getItem('regions'));
   const product = JSON.parse(localStorage.getItem('products'));
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const postPerPage = 10; //페이지당 포스트 개수
+  //현재 페이지 가져오기
+  const indexOfLastPage = currentPage * postPerPage; // 1 * 35 = 35번 포스트
+  const indexOfFirstPage = indexOfLastPage - postPerPage; // 35 - 35 = 0번 포스트
+  const [currentPosts, setCurrentPosts] = useState(category); //0~35번까지 포스트
+  //클릭 이벤트 페이지 바꾸기
+  const paginate = pageNum => setCurrentPage(pageNum);
+
   useEffect(() => {
     if (!isNaN(keyword)) {
       const findData = regions.state.data.find(
@@ -70,6 +80,12 @@ function ProductPage() {
     }
   }, [keyword]);
 
+  useEffect(() => {
+    if (category?.length > 0) {
+      setCurrentPosts(category.slice(indexOfFirstPage, indexOfLastPage));
+    }
+  }, [category, currentPage]);
+
   return (
     <Container>
       <Header />
@@ -78,15 +94,25 @@ function ProductPage() {
           <>
             <Regions regionData={target} />
             <ClothesBox>
-              {category.map(item => {
-                return <Clothes key={item.product_code} data={item} />;
-              })}
+              {currentPosts?.length > 0 ? (
+                currentPosts.map(item => {
+                  return <Clothes key={item.product_code} data={item} />;
+                })
+              ) : (
+                <div>검색 결과가 없습니다.</div>
+              )}
             </ClothesBox>
           </>
         ) : (
           <div>검색 결과가 없습니다.</div>
         )}
       </Body>
+      <Pagination
+        postPerPage={postPerPage}
+        totalPosts={130}
+        paginate={paginate}
+        currentPage={currentPage}
+      />
     </Container>
   );
 }
